ad4c8af257aeca63e3ab5c247a68093f72f61a26
irnikesh <60115084+irnikesh@users.noreply.github.com>
2025-03-11 13:58:10 +0100

    Update helpter_test_data.py

--- project-structure/utils/helpter_test_data.py
+++ project-structure/utils/helpter_test_data.py
@@ -1,1 +1,19 @@
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from databricks.sdk.runtime import spark
 
+# create data sets for testing 
+
+def create_test_data():
+    data_1 = [
+        ('1', '2022-07-01', 100.0, None, '0', 'plan', False, True, 0.5, 'id'),
+        ('2', '2022-07-01', 100.0, None, '0', 'plan', False, True, 0.5, 'id'),
+        ('3', '2023-01-01', 100.0, None, 0.0, 'plan', False, True, 0.5, 'id'),
+    ]
+    columns = ["id", "date_partition", "total_dollars", "churn_type", "change_dollars", "plan", "is_monthly", "is_yearly", "occupancy_rate", "customer_id"]
+    output = ( 
+                spark.createDataFrame(data_1, columns)
+                .withColumn("date_partition", F.col("date_partition").cast(T.DateType()))
+    )
+
+    return output
